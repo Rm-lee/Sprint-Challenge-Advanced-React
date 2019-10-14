d4faf7ea3d16f19750460218a3e051aa54ef1930
Roger Lee <rmlee0003@gmail.com>
2019-10-14 19:52:18 -0400

    created Person component to render have mapped data render each person details and added some styling

--- client/src/components/DisplayData.js
+++ client/src/components/DisplayData.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
+import Person from './Person'
 import axios from 'axios'
 class DisplayData extends Component {
  constructor(props) {
   super();
-  state = {
+  this.state = {
    playerData : []
   }
  }
  componentDidMount() {
-
+ axios.get('http://localhost:5000/api/players')
+ .then(res => {
+  this.setState({
+   playerData: res.data
+  })
+  console.log(this.state.playerData)
+ })
+ .catch(err => {
+  console.log(err)
+ })
  }
  render() {
   return (
    <div>
-    
+    {this.state.playerData.map(person => (
+    <Person person={person} />
+    ))}
    </div>
   );
  }
---
+++ client/src/components/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Person = (props) => {
+ return (
+  <div className='person'>
+   <p>Name: {props.person.name}</p>
+   <p>Country: {props.person.country}</p>
+   <p>Searches: {props.person.searches}</p>
+   
+  </div>
+ );
+};
+
+export default Person;
